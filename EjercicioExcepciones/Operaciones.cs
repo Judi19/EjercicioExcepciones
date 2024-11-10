084e5e75fe0c1fd30d2d2d27db9ba32f5bdf1970
Judi19 <167385372+Judi19@users.noreply.github.com>
2024-11-09 19:53:53 -0600

    Agregar archivos de proyecto.

---
+++ EjercicioExcepciones/Operaciones.cs
@@ -0,0 +1,26 @@
+﻿namespace EjercicioExcepciones
+{
+    public class Operaciones
+    {
+        public double Sumar(double Digito1, double Digito2)
+        {
+            return Digito1 + Digito2;
+        }
+        public double Restar(double Digito1, double Digito2)
+        {
+            return Digito1 - Digito2;
+        }
+        public double Multiplicar(double Digito1, double Digito2)
+        {
+            return Digito1 * Digito2;
+        }
+        public double Dividir(double Digito1, double Digito2)
+        {
+            if (Digito2 == 0)
+            {
+                throw new DivideByZeroException("No se puede dividir por cero");
+            }
+            return Digito1 / Digito2;
+        }
+    }
+}
